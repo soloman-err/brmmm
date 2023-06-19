22e56506c495962a0b1fe80919da151e4bc15e8b
solo <soloman.err@.com>
2023-06-19 23:51:57 +0600

    brmmm launched

---
+++ src/shared/footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      <small>&copy;brmmm All rights reserved.</small>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/shared/header/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <ul className="">
+        <li>Home</li>
+        <li>New Arrivals</li>
+        <li>Popular</li>
+        <li>Blogs</li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;

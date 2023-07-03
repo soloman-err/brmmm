78ddc28b776f5b44eb389013158e3d8996bbfb4b
solo <soloman.err@.com>
2023-07-03 22:52:34 +0600

    lazy-load implemented

--- src/pages/home/carousel/Carousel.jsx
+++ src/pages/home/carousel/Carousel.jsx
@@ -8,6 +8,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
 // import required modules
+import LazyLoad from 'react-lazy-load';
 import { Autoplay, Navigation, Pagination } from 'swiper';
 
 const Carousel = () => {
@@ -29,24 +30,30 @@ const Carousel = () => {
         className="mySwiper"
       >
         <SwiperSlide>
-          <img
-            src="https://image.adsoftheworld.com/u3b7uxnjm7pm0azkq9d2utkt2rlj"
-            alt="car-image-by-alexandercho"
-          />
+          <LazyLoad threshold={0.90}>
+            <img
+              src="https://image.adsoftheworld.com/u3b7uxnjm7pm0azkq9d2utkt2rlj"
+              alt="car-image-by-alexandercho"
+            />
+          </LazyLoad>
         </SwiperSlide>
 
         <SwiperSlide>
-          <img
-            src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/179937170661853.6462323b21bd9.jpg"
-            alt="car-image-by-alexandercho"
-          />
+          <LazyLoad>
+            <img
+              src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/179937170661853.6462323b21bd9.jpg"
+              alt="car-image-by-alexandercho"
+            />
+          </LazyLoad>
         </SwiperSlide>
 
         <SwiperSlide>
-          <img
-            src="https://cdn.myportfolio.com/afd63615b9a9590546acea6eac067ea3/06afedae28744d71ab7de98e_rw_1920.jpg?h=e79921e43fd27b14c8444952d6b03d78"
-            alt="car-image-by-alexandercho"
-          />
+          <LazyLoad>
+            <img
+              src="https://cdn.myportfolio.com/afd63615b9a9590546acea6eac067ea3/06afedae28744d71ab7de98e_rw_1920.jpg?h=e79921e43fd27b14c8444952d6b03d78"
+              alt="car-image-by-alexandercho"
+            />
+          </LazyLoad>
         </SwiperSlide>
       </Swiper>
     </>
--- src/shared/productCard/productCard.jsx
+++ src/shared/productCard/productCard.jsx
@@ -1,18 +1,21 @@
+import LazyLoad from 'react-lazy-load';
 import { Link } from 'react-router-dom';
 import './ProductCard.scss';
 
 const ProductCard = ({ product }) => {
-    
   return (
     <div id="item-card">
-    <img
-      src="https://dummyimage.com/600x500/000/111"
-      alt="product-card-image"
-    />
+      <LazyLoad width={'100%'} height={300} offset={20} threshold={0.50}>
+        <img
+          src="https://dummyimage.com/600x500/000/111"
+          alt="product-card-image"
+        />
+      </LazyLoad>
 
-    <Link to={`product-details/${product?._id}`}>
-    <button>View Details</button></Link>
-  </div>
+      <Link to={`product-details/${product?._id}`}>
+        <button>View Details</button>
+      </Link>
+    </div>
   );
 };
 
